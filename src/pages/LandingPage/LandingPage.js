efb35c79e92d88f9c874eed5016bbd90e41ea5bf
Chris Williams <cwill833@gmail.com>
2019-08-03 22:29:48 -0500

    white space gone on iphone screen

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,24 +1,25 @@
 import React, {Component} from 'react';
 import Controls from '../../components/Controls/Controls'
-import style from './LandingPage.module.css';
+import style from './LandingPage.module.css'
 
 class LandingPage extends Component{
 
-
-
   render(){
+
+    let classes = `${style.blueish} ${style.full}`
+    let otherClass = `${style.flex} ${style.full}`
     return (
       <div>
         <div className = {style.blueish}>
           <header>
             <h1>SLIDER</h1>
           </header>
-            <div className = {style.flex}>
-              <div className = {style.photo}>
-              </div>
+          <div className = {otherClass}>
+            <div className = {style.photo}>
             </div>
           </div>
-          <Controls />
+        </div>
+        <Controls />
       </div>
     );
   }
